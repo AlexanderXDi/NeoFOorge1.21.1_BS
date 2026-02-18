a45ab9ad0602b0b61b2ecf8a5c0fed05cc8b70b1
AlexanderXDi <157740633+AlexanderXDi@users.noreply.github.com>
2026-02-18 20:03:39 +0300

    Initial commit

---
+++ src/main/java/mopk/beer_snacks/Beer_snacks.java
@@ -0,0 +1,35 @@
+package mopk.beer_snacks;
+
+import static mopk.beer_snacks.CreativeTab.CREATIVE_MODE_TABS;
+import static mopk.beer_snacks.CreativeTab.MOD_TAB;
+import static mopk.beer_snacks.items.TabIcon.ITEMS;
+import static mopk.beer_snacks.items.TabIcon.TAB_ICON;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+
+
+@Mod(Beer_snacks.MODID)
+public class Beer_snacks {
+    public static final String MODID = "beer_snacks";
+
+
+    public Beer_snacks(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
+
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+
+        modEventBus.addListener(this::buildCreativeTabs);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    private void buildCreativeTabs(final BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == MOD_TAB.getKey()) {
+            event.accept(TAB_ICON);
+        }
+    }
+}
---
+++ src/main/java/mopk/beer_snacks/items/TabIcon.java
@@ -0,0 +1,14 @@
+package mopk.beer_snacks.items;
+
+import mopk.beer_snacks.Beer_snacks;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class TabIcon {
+    public static final DeferredRegister.Items ITEMS =
+            DeferredRegister.createItems(Beer_snacks.MODID);
+
+    public static final DeferredItem<Item> TAB_ICON = ITEMS.register("tab_icon",
+            () -> new Item(new Item.Properties()));
+}
